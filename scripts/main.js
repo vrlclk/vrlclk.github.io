37e040b440e038834d27d982296de62484d1cc83
vrlclk <74782116+vrlclk@users.noreply.github.com>
2022-04-28 14:04:46 +0200

    Fixes access to scripts and media

--- scripts/main.js
+++ scripts/main.js
@@ -6,12 +6,12 @@ myImage.onclick = function() {
     // gets the src attribute from the <img>-Element
     let mySrc = myImage.getAttribute('src');
     // checks if the photo is in the website
-    if(mySrc === '/test-site/images/bubatz-sheriff.jpg'){
+    if(mySrc === '/images/bubatz-sheriff.jpg'){
         // if yes, by clicking on the image, it should change to the other pic
-        myImage.setAttribute('src', '/test-site/images/bubatz-sheriff-but-derp.jpg');
+        myImage.setAttribute('src', '/images/bubatz-sheriff-but-derp.jpg');
     }else{
         // else, add the missing picture
-        myImage.setAttribute('src', '/test-site/images/bubatz-sheriff.jpg');
+        myImage.setAttribute('src', '/images/bubatz-sheriff.jpg');
     }
 }
 
